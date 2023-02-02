0a86de598e46034073932707a692b2508efc526f
YouHyunKim <yuhyunk7@gmail.com>
2023-02-02 15:28:44 +0900

    프로젝트 초기화

---
+++ src/main/java/shop/mtcoding/blog/homeController.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
