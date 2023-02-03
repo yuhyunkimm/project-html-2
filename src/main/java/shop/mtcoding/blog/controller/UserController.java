c3186a91248b648215336b220fe43aefe5e70ca3
YouHyunKim <yuhyunk7@gmail.com>
2023-02-03 15:10:14 +0900

    .xml / 1.회원가입 / 2.로그인 추가

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -1,14 +1,49 @@
 package shop.mtcoding.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.model.UserRepository;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/join")
+    public String join(String username, String password, String email) {
+        int result = userRepository.insert(username, password, email);
+        if (result == 1) {
+            return "redirect:/loginForm";
+        } else {
+            return "redirect:/joinForm";
+        }
+    }
+
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
+    }
+
+    @PostMapping("/login")
+    public String login(String usename, String password) {
+        User user = userRepository.findByUsernameAndPassword(usename, password);
+        if (user == null) {
+            return "redirect:/board";
+        } else {
+            session.setAttribute("principal", user);
+            return "redirect:/loginForm";
+
+        }
     }
 
     @GetMapping("/loginForm")
